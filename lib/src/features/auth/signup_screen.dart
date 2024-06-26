ee0b01416cbde91b446109efef0e2621501625dd
AronDevpro <140414272+AronDevpro@users.noreply.github.com>
2024-06-26 17:25:04 +0530

    fixed signup page

--- lib/src/features/auth/signup_screen.dart
+++ lib/src/features/auth/signup_screen.dart
@@ -1,86 +1,130 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_intern_project/src/common_widgets/header.dart';
 
-class SignupScreen extends StatelessWidget {
+class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+        title: const Text("Signup"),
+      ),
+      body: Container(
+        margin: const EdgeInsets.only(top:60.0),
         child: Column(
-          children: <Widget>[
-            const Header(),
-            const SizedBox(height: 24.0),
-            const Center(
-              child: Text(
-                'Sign Up',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Full Name',
                 ),
               ),
             ),
-            const SizedBox(height: 24.0),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Full Name',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  const Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
+            const Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'E-mail',
                       ),
-                      SizedBox(width: 16.0),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Contact Number',
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Contact Number',
                       ),
-                    ],
-                  ),
-                  const SizedBox(height: 16.0),
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(),
                     ),
-                    obscureText: true,
                   ),
-                  const SizedBox(height: 16.0),
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Confirm Password',
-                      border: OutlineInputBorder(),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Password',
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Confirm Password',
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: isChecked,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      isChecked = value!;
+                    });
+                  },
+                ),
+                const SizedBox(width: 8.0), // Adding some spacing between the Checkbox and the Text
+                const Text("I agree to the terms and conditions",style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),),
+              ],
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {Navigator.pushNamed(context, '/second');
+              },
+              child: const Text("Create Account"),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black,
                     ),
-                    obscureText: true,
                   ),
-                  const SizedBox(height: 32.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Add your signup logic here
-                    },
-                    child: const Text('Sign Up'),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text("OR",style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black,
+                    ),
                   ),
                 ],
               ),
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Already Have an Account? " ,style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),),
+                Text("Sign in", style: TextStyle(color: Colors.pink, fontWeight: FontWeight.w500, fontSize: 16.0),)
+              ],
             ),
           ],
         ),
